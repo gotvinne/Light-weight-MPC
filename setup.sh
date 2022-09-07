47dea39ab1391b8b6102f653cd8bb6d0791287f2
Geir Ola Tvinnereim <ge58muf@mytum.de>
2022-09-07 16:19:23 +0200

    Changed setup to detect build error

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash 
-
 cd build
 
 conan install ..
 cmake ..
 make
-./light_weight
+
+if [ $? -eq 0 ]; then
+    echo 
+    ./bin/light_weight
+else
+    RED='\033[0;31m'
+    NC='\033[0m' 
+    echo "${RED}BUILD ERROR! ${NC}\n"
+fi
