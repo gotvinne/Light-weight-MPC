70a47f0446f401b03ea84777c0f9f8d46571ec21
Geir Ola Tvinnereim <ge58muf@mytum.de>
2022-09-07 09:34:52 +0200

    Implemented Cmake environment printing Hello world

---
+++ build/CMakeFiles/light_weight.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/light_weight.dir/src/main.cc.o"
+  "light_weight"
+  "light_weight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/light_weight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cc
@@ -0,0 +1,10 @@
+// Copyright (C) Geir Ola Tvinnereim 2022
+
+#include <iostream>
+
+
+int main() {
+
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
