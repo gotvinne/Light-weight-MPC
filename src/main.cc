0881a594f73593f716cb21ec05f0f82f4d84b8e1
Geir Ola Tvinnereim <ge58muf@mytum.de>
2022-10-11 13:25:03 +0200

    Push for project planning

--- src/main.cc
+++ src/main.cc
@@ -20,20 +20,21 @@ int main() {
     //std::vector<float> vec = step_coefficients(k, tau, theta, N);
     //print_coefficients(vec);
     
-
     // json data = parse("../scenarios/test.json");
     // std::cout << data.dump() << std::endl;
 
     // // Flow: 
+    // 1)
     // std::string scenario {"../scenarios/siso_test.json"};
-
     // // Read scenario and check if it is valid
+
+    // 2)
     // // Simulate scenario returning simulation
 
+    // 3)
     // // Write simulation to an output file
 
-
-    // // Evt. 
+    // 4)
     // // Call python script to read data and plot simulation. 
   
     return 0;
