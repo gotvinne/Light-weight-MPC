70cc1c112164e9da55caba9b739db9f881a93b01
Geir Ola <gotvinne@stud.ntnu.no>
2022-10-23 19:57:55 +0200

    Added logic detecting runtime error

--- setup.sh
+++ setup.sh
@@ -9,10 +9,17 @@ cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX
 make
 
-if [ $? -eq 0 ]; then
+# $? denote exit code
+if [ $? -eq 0 ]; then #Build successful
     echo 
     ./light_weight
-    echo
+    if [ $? -eq 0 ]; then #Running successfull
+        echo
+    else
+        RED='\033[0;31m'
+        NC='\033[0m' 
+        echo "${RED}RUNTIME ERROR! ${NC}\n"
+    fi
 else
     RED='\033[0;31m'
     NC='\033[0m' 
