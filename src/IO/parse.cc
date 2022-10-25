ca0cfc0bd017fa726e83a77ad3fb764b1b44558a
Geir Ola <gotvinne@stud.ntnu.no>
2022-10-25 22:00:39 +0200

    Json parser with some error handling

--- src/IO/parse.cc
+++ src/IO/parse.cc
@@ -16,6 +16,8 @@
 #include <fstream>
 #include <vector>
 
+#include <stdexcept>
+
 using json = nlohmann::json; 
 
 json ReadJson(const std::string& filepath) {
@@ -40,14 +42,17 @@ void ModelData(const json& sys_data, std::map<std::string,int>& map) {
         std::cout << e.what() << std::endl; 
     }    
 }
+
 /* Error handling:
     Check that S is correct with N_MV and N_CV
     Check that number of S is correct with n_CV
 */
-
 StateData::StateData() {}
 StateData::StateData(const json& cv_data, int n_MV, int n_CV, int N) {
     int n_states = cv_data.size();
+    if (n_states != n_CV) {
+        throw std::invalid_argument("n_CV does not coincide with CV");
+    }
 
     S.resize(n_MV, N*n_CV);
     for (int states = 0; states < n_CV; states++) {
@@ -68,12 +73,13 @@ void FillStateCoMatrix(const json& s_data, Eigen::MatrixXf& S, int n_MV, int sta
     }       
 }
 
-// Need to customize input data, to generate datapoints based on mpc_horizon.
-// Check that n_inputs == n_MV!
 // Check that T < then number of reference points in data. 
 InputData::InputData() {}
 InputData::InputData(const json& mv_data, int n_MV, int T) {
     int n_inputs = mv_data.size();
+    if (n_inputs != n_MV) {
+        throw std::invalid_argument("n_MV does not coincide with MV");
+    }
 
     Ref.resize(T*n_MV);
     for (int inputs = 0; inputs < n_MV; inputs++) {
@@ -81,6 +87,9 @@ InputData::InputData(const json& mv_data, int n_MV, int T) {
         Input.push_back(input_data.at(kInput));
         Init.push_back(input_data.at(kInit));
 
+        if (input_data.at(kU).size() < T) {
+            throw std::invalid_argument("Too few input data for the horizon");
+        }
         FillReference(input_data.at(kU), Ref, T*inputs, T);
     }             
 }
@@ -93,63 +102,66 @@ void FillReference(const json& ref_data, Eigen::ArrayXf& ref, int start_index, i
 
 MPCConfig::MPCConfig() : P(), M(), W(), Ro(), bias_update() {}
 MPCConfig::MPCConfig(const json& sce_data, int n_CV, int n_MV) {
-    try {
-        json mpc_data = sce_data.at(kMPC);
-        P = mpc_data.at(kP);
-        M = mpc_data.at(kM);
-        W = mpc_data.at(kW);
+    json mpc_data = sce_data.at(kMPC);
+    P = mpc_data.at(kP);
+    M = mpc_data.at(kM);
+    W = mpc_data.at(kW);
 
-        Q.resize(n_CV); 
-        R.resize(n_MV);
-        for (int i = 0; i < n_CV; i++) {
-            Q[i] = mpc_data.at(kQ).at(i);
-        }
-        for (int i = 0; i < n_MV; i++) {
-            R[i] = mpc_data.at(kR).at(i);
-        }
-
-        Ro = mpc_data.at(kRo);
-        bias_update = mpc_data.at(kBu);
+    Q.resize(n_CV); 
+    R.resize(n_MV);
+    for (int i = 0; i < n_CV; i++) {
+        Q[i] = mpc_data.at(kQ).at(i);
     }
-    catch(json::exception& e) {
-        std::cout << e.what() << std::endl; 
-    } 
+    for (int i = 0; i < n_MV; i++) {
+        R[i] = mpc_data.at(kR).at(i);
+    }
+
+    Ro = mpc_data.at(kRo);
+    bias_update = mpc_data.at(kBu);
 }
 
 void ConstraintData(const json& sce_data, Eigen::ArrayXf& arr, bool upper) {
-    try {
-        json j_arr(sce_data.at(kC));
-        int size = j_arr.size();
+    json j_arr(sce_data.at(kC));
+    int size = j_arr.size();
 
-        arr.resize(size);
-        for (int i = 0; i < size; i++) {
-            for (auto& elem : j_arr.at(i).items()) {
-                arr[i] = elem.value().at(upper);
-            }
+    arr.resize(size);
+    for (int i = 0; i < size; i++) {
+        for (auto& elem : j_arr.at(i).items()) {
+            arr[i] = elem.value().at(upper);
         }
     }
-    catch(json::exception& e) {
-        std::cout << e.what() << std::endl; 
-    } 
 }
 
 void ParseScenarioData(const json& sce_data, std::string& system, MPCConfig& mpc_config, 
                         Eigen::ArrayXf& upper_constraints, Eigen::ArrayXf& lower_constraints,
                         int n_CV, int n_MV) {
-    system = sce_data.at(kSystem);
-    mpc_config = MPCConfig(sce_data, n_CV, n_MV);
-    ConstraintData(sce_data, upper_constraints, true);
-    ConstraintData(sce_data, lower_constraints, false);
+    try {                     
+        system = sce_data.at(kSystem);
+        mpc_config = MPCConfig(sce_data, n_CV, n_MV);
+        ConstraintData(sce_data, upper_constraints, true);
+        ConstraintData(sce_data, lower_constraints, false);
+    }
+    catch(json::exception& e) {
+        std::cerr << "ERROR! " << e.what() << std::endl; 
+    } 
 }
 
 void ParseSystemData(const json& sys_data, std::map<std::string, int>& model_param,
                     StateData& state_data, InputData& input_data, int T) {
-    ModelData(sys_data, model_param);
-    json cv_data = sys_data.at(kCV);
-    json mv_data = sys_data.at(kMV);
+    try {
+        ModelData(sys_data, model_param);
+        json cv_data = sys_data.at(kCV);
+        json mv_data = sys_data.at(kMV);
 
-    state_data = StateData(cv_data, model_param[kN_MV], model_param[kN_CV], model_param[kN]);
-    input_data = InputData(mv_data, model_param[kN_MV], T); 
+        state_data = StateData(cv_data, model_param[kN_MV], model_param[kN_CV], model_param[kN]);
+        input_data = InputData(mv_data, model_param[kN_MV], T); 
+    }
+    catch(json::exception& e) {
+        std::cerr << "ERROR! " << e.what() << std::endl; 
+    } 
+    catch(std::invalid_argument& e) {
+        std::cerr << "ERROR! " << e.what() << std::endl; 
+    }
 }
 
 void PrintContainer(std::map<std::string,int> container) {
