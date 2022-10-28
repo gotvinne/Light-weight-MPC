1728d6d8aec4c7413f50047799a2e32d925d40c3
Geir Ola <gotvinne@stud.ntnu.no>
2022-10-28 20:25:37 +0200

    Began implmenting reference model

---
+++ src/model/reference_model.cc
@@ -0,0 +1,15 @@
+/**
+ * @file reference_model.cc
+ * @author Geir Ola Tvinnereim
+ * @copyright  Geir Ola Tvinnereim 
+ * @date 2022
+ */
+
+#include "reference_model.h"
+#include <random>
+
+float WhiteNoise(float sigma) {
+    std::default_random_engine generator;
+    std::normal_distribution<float> dist(WHITE_NOISE_MEAN, sigma);
+    return dist(generator);
+}
