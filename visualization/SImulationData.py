48b5a8f0cddebb0cb641af061210ac4e521a1a70
Geir Ola <gotvinne@stud.ntnu.no>
2022-10-30 23:26:22 +0100

    Tested SimulationData

--- visualization/SImulationData.py
+++ visualization/SImulationData.py
@@ -2,6 +2,7 @@
 import numpy as np
 import json
 import os
+from pathlib import Path
 
 # Simulation file specifiers
 SCENARIO = "scenario"
@@ -11,9 +12,9 @@ N_MV = "n_MV"
 
 CV = "CV"
 MV = "MV"
-OUTPUT = "Output"
-INPUT = "Input"
-UNIT = "Unit"
+OUTPUT = "output"
+INPUT = "input"
+UNIT = "unit"
 CONSTRAINT = "c"
 Y = "y"
 Y_HAT = "y_hat"
@@ -21,8 +22,8 @@ U = "u"
 
 class SimulationData():
     def __init__(self, filepath):
-
-        json_data = self.ReadSimulation(filepath)
+        simulation_filepath = str(Path(__file__).parent.parent) + filepath # Relative filepath
+        json_data = self.ReadSimulation(simulation_filepath)
 
         self.scenario = json_data[SCENARIO]
         self.T = json_data[T]
@@ -47,40 +48,35 @@ class SimulationData():
 
     def ParseCVData(self, data):
         cv_data = data[CV]
-
-        self.outputs = []
-        self.cv_units = []
-        self.cv_constraints = []
+        outputs, cv_units, cv_constraints = [], [], []
         for index, elem in enumerate(cv_data):
-            self.outputs.append(elem[OUTPUT])
-            self.cv_units.append(elem[UNIT])
-            self.cv_constraints.append((elem[CONSTRAINT][0], elem[CONSTRAINT][1]))
+            outputs.append(elem[OUTPUT])
+            cv_units.append(elem[UNIT])
+            cv_constraints.append((elem[CONSTRAINT][0], elem[CONSTRAINT][1]))
 
             # Fill nparray
             self.y.T[..., index] = elem[Y]
             self.y_hat.T[..., index] = elem[Y_HAT]
+        self.outputs, self.cv_units, self.cv_constraints = outputs, cv_units, cv_constraints
 
     def ParseMVData(self, json_data):
         mv_data = json_data[MV]
-
-        self.inputs = []
-        self.mv_units = []
-        self.mv_constraints = []
-
+        inputs, mv_units, mv_constraints = [], [], []
         for index, elem in enumerate(mv_data):
-            self.inputs.append(elem[OUTPUT])
-            self.mv_units.append(elem[UNIT])
-            self.mv_constraints.append((elem[CONSTRAINT][0], elem[CONSTRAINT][1]))
+            inputs.append(elem[INPUT])
+            mv_units.append(elem[UNIT])
+            mv_constraints.append((elem[CONSTRAINT][0], elem[CONSTRAINT][1]))
 
             self.u.T[..., index] = elem[U]
+        self.inputs, self.mv_units, self.mv_constraints = inputs, mv_units, mv_constraints
 
 def ListSimulations(sim_dirpath):
     """
 
     :return:
     """
-    simulation_filepath = os.path.abspath(os.curdir) + sim_dirpath
+    simulation_filepath = str(Path(__file__).parent.parent) + sim_dirpath
     files = os.listdir(simulation_filepath)
 
     for simulation in files:
-        print(simulation)
+        print(sim_dirpath + "/" + simulation)
--- visualization/plot.py
+++ visualization/plot.py
@@ -1,10 +1,14 @@
 
 from matplotlib import pyplot as plt
+from SimulationData import *
+import numpy as np
 
-def PlotPrediction(n_CV):
-    fig, ax = plt.subplots(nrows=n_CV)
-    ax.set(xlabel='T', ylabel='System values', title='Visualization of MPC prediction')
-    ax.grid()
-    plt.show()
+def PlotPrediction(sim_data):
+    fig, axs = plt.subplots(nrows=sim_data.n_CV)
+    t = np.arange(0, sim_data.T, dtype=int)
+    for i in range(sim_data.n_CV):
+        axs[i, i].set(xlabel='T', ylabel=sim_data.cv_units[i], title='Visualization of MPC prediction')
+        axs.grid()
+        plt.show()
     
 
