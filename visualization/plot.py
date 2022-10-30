97528a233d36bf228ee87937937ebc3abea406b7
Geir Ola <gotvinne@stud.ntnu.no>
2022-10-31 00:35:52 +0100

    Implemented light_weight visualization

--- visualization/plot.py
+++ visualization/plot.py
@@ -1,14 +1,35 @@
 
 from matplotlib import pyplot as plt
-from SimulationData import *
-import numpy as np
 
-def PlotPrediction(sim_data):
-    fig, axs = plt.subplots(nrows=sim_data.n_CV)
+from SimulationData import *
+
+def PlotPrediction(sim_data, title):
     t = np.arange(0, sim_data.T, dtype=int)
     for i in range(sim_data.n_CV):
-        axs[i, i].set(xlabel='T', ylabel=sim_data.cv_units[i], title='Visualization of MPC prediction')
-        axs.grid()
-        plt.show()
-    
+        plt.subplot(1, sim_data.n_CV, i+1)
+        plt.plot(t, sim_data.y[i, :], "b", label="System output")
+        plt.plot(t, sim_data.y_hat[i, :], "m", label="Predicted output")
 
+        plt.xlabel("MPC horizon, t")
+        plt.ylabel(sim_data.cv_units[i])
+        plt.legend()
+
+        plt.title(sim_data.outputs[i])
+        plt.grid()
+    plt.suptitle(title)
+    plt.show()
+    
+def PlotInput(sim_data, title):
+    t = np.arange(0, sim_data.T, dtype=int)
+    for i in range(sim_data.n_MV):
+        plt.subplot(1, sim_data.n_MV, i + 1)
+        plt.plot(t, sim_data.u[i, :], "b", label="Optimized actuation")
+
+        plt.xlabel("MPC horizon, t")
+        plt.ylabel(sim_data.mv_units[i])
+        plt.legend()
+
+        plt.title(sim_data.inputs[i])
+        plt.grid()
+    plt.suptitle(title)
+    plt.show()
