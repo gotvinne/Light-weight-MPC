66c20e7debb4c3991ed2f49ad9bc0f073bc23a10
Geir Ola <gotvinne@stud.ntnu.no>
2022-11-08 12:43:35 +0100

    Documendted visualization module

--- visualization/SimulationData.py
+++ visualization/SimulationData.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import json
 import os
@@ -20,9 +19,14 @@ Y = "y"
 Y_HAT = "y_hat"
 U = "u"
 
+
 class SimulationData():
     def __init__(self, filepath):
-        simulation_filepath = str(Path(__file__).parent.parent) + filepath # Relative filepath
+        """
+        Constructor for the SimulationData object parsing a simulation file
+        :param filepath: The local filepath to simulation data
+        """
+        simulation_filepath = str(Path(__file__).parent.parent) + filepath  # Relative filepath
         json_data = self.ReadSimulation(simulation_filepath)
 
         self.scenario = json_data[SCENARIO]
@@ -39,15 +43,19 @@ class SimulationData():
 
     def ReadSimulation(self, filepath):
         """
-
-        :param filepath:
-        :return:
+        Read simulation data json file
+        :param filepath: relative path to simulation data
+        :return: a json object holding the parsed simulation data
         """
         with open(filepath, "r") as file:
             return json.load(file)
 
-    def ParseCVData(self, data):
-        cv_data = data[CV]
+    def ParseCVData(self, json_data):
+        """
+        Parsing the controlled variable simulation
+        :param json_data: json object holding parsed simulation data
+        """
+        cv_data = json_data[CV]
         outputs, cv_units, cv_constraints = [], [], []
         for index, elem in enumerate(cv_data):
             outputs.append(elem[OUTPUT])
@@ -60,6 +68,10 @@ class SimulationData():
         self.outputs, self.cv_units, self.cv_constraints = outputs, cv_units, cv_constraints
 
     def ParseMVData(self, json_data):
+        """
+        Parsing the manipulated variable simulation
+        :param json_data: json object holding parsed simulation data
+        """
         mv_data = json_data[MV]
         inputs, mv_units, mv_constraints = [], [], []
         for index, elem in enumerate(mv_data):
@@ -70,10 +82,11 @@ class SimulationData():
             self.u.T[..., index] = elem[U]
         self.inputs, self.mv_units, self.mv_constraints = inputs, mv_units, mv_constraints
 
+
 def ListSimulations(sim_dirpath):
     """
-
-    :return:
+    Listing all avaliable simulations
+    :param sim_dirpath: Local directory path
     """
     simulation_filepath = str(Path(__file__).parent.parent) + sim_dirpath
     files = os.listdir(simulation_filepath)
--- visualization/plot.py
+++ visualization/plot.py
@@ -1,12 +1,17 @@
-
 from matplotlib import pyplot as plt
 
 from SimulationData import *
 
+
 def PlotPrediction(sim_data, title):
+    """
+    Plotting the n_CV different predicted outputs along with the reference model output
+    :param sim_data: SimulationData object holding the simulation information
+    :param title: The main title of the plots
+    """
     t = np.arange(0, sim_data.T, dtype=int)
     for i in range(sim_data.n_CV):
-        plt.subplot(1, sim_data.n_CV, i+1)
+        plt.subplot(1, sim_data.n_CV, i + 1)
         plt.plot(t, sim_data.y[i, :], "b", label="System output")
         plt.plot(t, sim_data.y_hat[i, :], "m", label="Predicted output")
 
@@ -18,8 +23,14 @@ def PlotPrediction(sim_data, title):
         plt.grid()
     plt.suptitle(title)
     plt.show()
-    
+
+
 def PlotInput(sim_data, title):
+    """
+    Plotting the n_MV different optimized inputs
+    :param sim_data: SimulationData object holding the simulation information
+    :param title: The main title of the plots
+    """
     t = np.arange(0, sim_data.T, dtype=int)
     for i in range(sim_data.n_MV):
         plt.subplot(1, sim_data.n_MV, i + 1)
