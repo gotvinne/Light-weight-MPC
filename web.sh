523b99da2143c8c2027925180ea7a14b38b19c49
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2023-03-21 19:24:47 +0100

    Updated em compiling routine

--- web.sh
+++ web.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+# Delete old files:
+rm -rf build
+rm -rf public
+
+# Make folders
 mkdir -p "build"
 mkdir -p "public"
 cd build
 
 # Building Release
-emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DWEB=ON
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DWEB=ON -DC_FUNCTIONS:STRING='_add'
 make 
