50520a59ac88d5db4c66d31ea5fab1fdb6ca70e7
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2023-04-18 17:54:29 +0200

    Cleared local storage for server start-up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer"
 import "./css/App.css"
 
 function App() {  
+  localStorage.clear(); // Clear local storage when starting server
   return (    
   <div className="App"> 
       <div className="Header">
