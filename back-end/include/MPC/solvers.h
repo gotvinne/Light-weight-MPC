6610db9125455c6de8c4cc3c7e1045cab951ee21
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2023-04-19 19:36:19 +0200

    ^

---
+++ back-end/include/MPC/solvers.h
@@ -0,0 +1,36 @@
+/**
+ * @file solvers.h
+ * @author Geir Ola Tvinnereim
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-19
+ * 
+ * @copyright Released under the terms of the BSD 3-Clause License
+ * 
+ */
+#ifndef SOLVERS_H
+#define SOLVERS_H
+
+#include "model/FSRModel.h"
+#include "IO/data_objects.h"
+
+#include <Eigen/Eigen>
+
+using VectorXd = Eigen::VectorXd;
+using MatrixXd = Eigen::MatrixXd;
+/**
+ * @brief Solving the condensed optimalization problem using OSQP-Eigen
+ * 
+ * @param T MPC horizon
+ * @param u_mat Optimized u, filled by reference
+ * @param y_pred Predicted y, filled by reference
+ * @param fsr FSRModel, finite step response model 
+ * @param conf MPC configuration
+ * @param z_min lower constraint vector
+ * @param z_max upper constraint vector
+ * @param y_ref Output reference data
+ */
+void SRSolver(int T, MatrixXd& u_mat, MatrixXd& y_pred, FSRModel& fsr, const MPCConfig& conf, const VectorXd& z_min, 
+             const VectorXd& z_max, VectorXd* y_ref);
+
+#endif // SOLVERS_H
