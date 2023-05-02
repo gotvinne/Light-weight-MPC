296b2ba93f0c3965281f8bc1a0dc77737b660006
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2023-05-02 13:25:36 +0200

    Plotted constraints

--- front-end/src/utils/plot.js
+++ front-end/src/utils/plot.js
@@ -4,17 +4,39 @@ import Plot from "react-plotly.js"
 
 export function PlotPrediction(cv_data, T) {
     let t = Array.from({ length: T }, (value, index) => index);
+    let lower = Array(T).fill(cv_data.c[0]);
+    let upper = Array(T).fill(cv_data.c[1]);
 
-    var data = [
-        {
-            x: t,
-            y: cv_data.y_pred,
-            type: "line",
-            line: {
-                color: 'rgb(128, 0, 128)'
-            }
+    var prediction = {
+        x: t,
+        y: cv_data.y_pred,
+        type: "line",
+        line: {
+            color: 'rgb(128, 0, 128)'
         }
-    ];
+    };
+
+    var lower_constraint = {
+        x: t, 
+        y: lower,
+        mode: 'lines',
+        line: {
+            dash: 'dot',
+            width: 2,
+            color: 'rgb(0, 0, 0)'
+        }
+    } 
+
+    var upper_constraint = {
+        x: t, 
+        y: upper,
+        mode: 'lines',
+        line: {
+            dash: 'dot',
+            width: 2,
+            color: 'rgb(0, 0, 0)'
+        }
+    } 
 
     var layout = {
         title: cv_data.output,
@@ -27,23 +49,45 @@ export function PlotPrediction(cv_data, T) {
     };
 
     return (
-        <Plot data={data} layout={layout}/>
+        <Plot data={[prediction, lower_constraint, upper_constraint]} layout={layout}/>
     ); 
 }
 
 export function PlotActuation(mv_data, T) {
     let t = Array.from({ length: T }, (value, index) => index);
+    let lower = Array(T).fill(mv_data.c[0]);
+    let upper = Array(T).fill(mv_data.c[1]);
 
-    var data = [
-        {
-            x: t,
-            y: mv_data.u,
-            type: "line",
-            line: {
-                color: 'rgb(0, 0, 255)'
-            }
+    var actuation = {
+        x: t,
+        y: mv_data.u,
+        type: "line",
+        line: {
+            color: 'rgb(0, 0, 255)'
         }
-    ];
+    };
+
+    var lower_constraint = {
+        x: t, 
+        y: lower,
+        mode: 'lines',
+        line: {
+            dash: 'dot',
+            width: 2,
+            color: 'rgb(0, 0, 0)'
+        }
+    } 
+
+    var upper_constraint = {
+        x: t, 
+        y: upper,
+        mode: 'lines',
+        line: {
+            dash: 'dot',
+            width: 2,
+            color: 'rgb(0, 0, 0)'
+        }
+    } 
 
     var layout = {
         title: mv_data.input,
@@ -56,6 +100,6 @@ export function PlotActuation(mv_data, T) {
     };
 
     return (
-        <Plot data={data} layout={layout}/>
+        <Plot data={[actuation, lower_constraint, upper_constraint]} layout={layout}/>
     ); 
 }
