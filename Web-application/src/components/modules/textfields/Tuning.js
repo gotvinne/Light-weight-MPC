e469c25a6dc6c8570cfae171f25d936b9e1a0db4
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2023-06-07 12:16:17 +0200

    Added letters to tuning

--- Web-application/src/components/modules/textfields/Tuning.js
+++ Web-application/src/components/modules/textfields/Tuning.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Box, Typography, TextField } from '@mui/material';
+import { BlockMath, InlineMath } from 'react-katex';
+
+const SYMBOLS = [`P =`, `M =`,`W =`,`Q =`,`R =`,`\\rho_h =`,`\\rho_l =`];
 
 /**
  * Displaying the MPC tuning TextFields
@@ -17,22 +20,43 @@ export default function Tuning({ncv, nmv, error, sce, handler}) {
             </Box>
         
             <Box sx={{pt: 2, display: "flex", flexDirection: "row"}}>
-                <Box sx ={{pt:2, pl: "13%"}}>
-                    <TextField error={error["P"]} id={"P"} variant="outlined" helperText={"Prediction horizon P, int"} value={sce["P"]} onChange={handler} required/>
+                <Box sx ={{pt:2, pl: "3%"}}>
+                    <Box sx= {{display: "flex", flexDirection: "colum"}}> 
+                        <BlockMath sx={{pt: "2%"}} math={SYMBOLS[0]} />
+                        <TextField sx={{pl:"2%"}} error={error["P"]} id={"P"} variant="outlined" helperText={"Prediction horizon, int"} value={sce["P"]} onChange={handler} required/>
+                    </Box>
                     <Box />
-                    <TextField error={error["M"]} id={"M"} variant="outlined" helperText={"Control horizon M, int"} value={sce["M"]} onChange={handler} required/>
+                    <Box sx= {{display: "flex", flexDirection: "colum"}}> 
+                        <BlockMath sx={{pt: "2%"}} math={SYMBOLS[1]} />
+                        <TextField sx={{pl:"2%"}} error={error["M"]} id={"M"} variant="outlined" helperText={"Control horizon, int"} value={sce["M"]} onChange={handler} required/>
+                    </Box>
                     <Box />
-                    <TextField error={error["W"]} id={"W"} variant="outlined" helperText={"Time delay, W, int"} value={sce["W"]} onChange={handler} required/>
+                    <Box sx= {{display: "flex", flexDirection: "colum"}}> 
+                        <BlockMath sx={{pt: "2%"}} math={SYMBOLS[2]} />
+                        <TextField sx={{pl:"2%"}} error={error["W"]} id={"W"} variant="outlined" helperText={"Start horizon, int"} value={sce["W"]} onChange={handler} required/>
+                    </Box>
                 </Box>
 
-                <Box sx={{pt:2, pl: 2}}>
-                    <TextField error={error["Q"]} id={"Q"} variant="outlined" helperText={"Q, vector<double>, length: "+ ncv.toString()} value={sce["Q"]} onChange={handler} required/>
+                <Box sx={{pt:2, pl: "3%"}}>
+                    <Box sx= {{display: "flex", flexDirection: "colum"}}> 
+                        <BlockMath sx={{pt: "2%"}} math={SYMBOLS[3]} />
+                        <TextField sx={{pl:"2%"}} error={error["Q"]} id={"Q"} variant="outlined" helperText={"vector<double>, length: "+ ncv.toString()} value={sce["Q"]} onChange={handler} required/>
+                    </Box>
                     <Box />
-                    <TextField error={error["R"]} id={"R"} variant="outlined" helperText={"R, vector<double>, length: "+ nmv.toString()} value={sce["R"]} onChange={handler} required/>
+                    <Box sx= {{display: "flex", flexDirection: "colum"}}> 
+                        <BlockMath sx={{pt: "2%"}} math={SYMBOLS[4]} />
+                        <TextField sx={{pl:"2%"}} error={error["R"]} id={"R"} variant="outlined" helperText={"vector<double>, length: "+ nmv.toString()} value={sce["R"]} onChange={handler} required/>
+                    </Box>
                     <Box />
-                    <TextField error={error["RoH"]} id={"RoH"} variant="outlined" helperText={"RoH, vector<double>, length: "+ ncv.toString()} value={sce["RoH"]} onChange={handler} required/>
+                    <Box sx= {{display: "flex", flexDirection: "colum"}}> 
+                        <BlockMath sx={{pt: "2%"}} math={SYMBOLS[5]} />
+                        <TextField sx={{pl:"2%"}} error={error["RoH"]} id={"RoH"} variant="outlined" helperText={"vector<double>, length: "+ ncv.toString()} value={sce["RoH"]} onChange={handler} required/>
+                    </Box>
                     <Box />
-                    <TextField error={error["RoL"]} id={"RoL"} variant="outlined" helperText={"RoL, vector<double>, length: "+ ncv.toString()} value={sce["RoL"]} onChange={handler} required/>
+                    <Box sx= {{display: "flex", flexDirection: "colum"}}> 
+                        <BlockMath sx={{pt: "2%"}} math={SYMBOLS[6]} />
+                        <TextField sx={{pl:"2%"}} error={error["RoL"]} id={"RoL"} variant="outlined" helperText={"vector<double>, length: "+ ncv.toString()} value={sce["RoL"]} onChange={handler} required/>
+                    </Box>
                 </Box>
             </Box>
         </div>
