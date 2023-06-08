f4b3d9d2ed6e4210669bee34ff011e75ce907c93
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2023-06-08 23:09:45 +0200

    Changed title in vis tool

--- MPC-simulator/vis/plot.py
+++ MPC-simulator/vis/plot.py
@@ -18,13 +18,15 @@ if (args.simulation[0:13] == "sim_open_loop"):
 simulation_name = "/data/simulations/" + args.simulation + ".json"
 if open_loop:
     sim_data = OpenLoopData(simulation_name)
+    title = "Open loop simulation, CV (purple) MV (blue), " + args.simulation
 else:
     sim_data = SimulationData(simulation_name)
+    title = "MPC simulation data, CV (purple) MV (blue), " + args.simulation
 
 fontsize = 12
 figsize = 12
 plt.rcParams.update({'font.size': fontsize})
-title = "MPC simulation data, CV (purple) MV (blue)"
+
 
 PlotMPC(sim_data, title, figsize)
 input("")
